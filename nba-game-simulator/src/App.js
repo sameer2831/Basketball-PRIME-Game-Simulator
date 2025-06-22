36312eb64e8054cedd4a9fe516f43dbda8dc858b
sameer2831 <sameerb2831@gmail.com>
2025-06-22 00:26:56 -0400

    git pages host updation

--- nba-game-simulator/src/App.js
+++ nba-game-simulator/src/App.js
@@ -1,10 +1,18 @@
 import { TeamProvider } from './context/TeamContext';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
+// example page
 
 function App() {
   return (
     <TeamProvider>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+         
+        </Routes>
+      </Router>
     </TeamProvider>
   );
 }
